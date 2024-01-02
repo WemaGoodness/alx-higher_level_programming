2ec72c31b2e0137e3d6fd5a27acfff25212039c3
WemaGoodness <goodnesswema20@gmail.com>
2024-01-02 19:17:15 +0300

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if not (isinstance(a, int) or isinstance(a, float) or not isinstance(b, int)
-    or isinstance(b, float)):
+    if not (isinstance(a, int) or isinstance(a, float)
+            or not isinstance(b, int) or isinstance(b, float)):
         raise ValueError("Both values must be integers")
     result = a ** b
 
