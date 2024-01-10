5a2d6378416bca9bf2f6c1fa901e79761fcd1828
WemaGoodness <goodnesswema20@gmail.com>
2024-01-10 21:36:39 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dictionary = {}
-    for key, value in a_dictionary.items():
-        new_dictionary[key] = value * 2
-    return new_dictionary
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
