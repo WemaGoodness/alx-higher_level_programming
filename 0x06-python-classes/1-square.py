9b79fa063cf075b5f15e19443ad075634313e2c4
WemaGoodness <goodnesswema20@gmail.com>
2024-01-24 01:02:25 +0300

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""This module defines a class Square"""
+
+
 class Square:
+    """This class represents a square with a private instance attribute size"""
+
     def __init__(self, size):
+        """This method initializes the square with a given size"""
         self.__size = size
+
