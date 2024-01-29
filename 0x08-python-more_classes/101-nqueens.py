37b010aeb442cb38f76c3991ac66e451c7f2dc92
WemaGoodness <goodnesswema20@gmail.com>
2024-01-29 23:13:01 +0300

    Python - More Classes and Objects

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,50 @@
+import sys
+
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i] == j:
+                solution.append([i, j])
+    print(solution)
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[i] == row or \
+            board[i] - i == row - col or \
+            board[i] + i == row + col:
+            return False
+    return True
+
+def solve_nqueens(board, col):
+    n = len(board)
+    if col == n:
+        print_solution(board)
+        return
+
+    for i in range(n):
+        if is_safe(board, i, col):
+            board[col] = i
+            solve_nqueens(board, col + 1)
+
+def check_args():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
+
+def main():
+    n = check_args()
+    board = [-1] * n
+    solve_nqueens(board, 0)
+
+if __name__ == "__main__":
+    main()
